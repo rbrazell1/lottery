874e48c25e39ded8ee725754657d51fc0d95f33d
rbrazell1 <brazellrussell@gmail.com>
2021-03-04 22:28:02 -0700

    Added NumberBase with test

---
+++ src/main/java/edu/cnm/deepdive/exam1/prep/NumberBase.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive.exam1.prep;
+
+
+public class NumberBase {
+
+  private static char[] digits = {
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+      'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+      'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+      'v', 'w', 'x', 'y', 'z'
+  };
+
+  public NumberBase() {
+
+  }
+
+  public static String convertToBase(long value, int radix) {
+    String rep = "";
+    long n = Math.abs(value);
+    while (n != 0) {
+      int remainder = (int) (n % radix);
+      rep = digits[remainder] + rep;
+      n = n / radix;
+    }
+
+    return rep;
+  }
+}
---
+++ src/test/java/edu/cnm/deepdive/exam1/prep/NumberBaseTest.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.exam1.prep;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NumberBaseTest {
+
+  @Test
+  void convertToBase() {
+    return String;
+  }
+}
